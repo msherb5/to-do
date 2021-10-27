db0a6159e9f1dfb3ac1cf9c77edd0679acc3686b
Michael Sherbine <56451699+msherb5@users.noreply.github.com>
2021-10-27 10:03:55 -0400

    Add files via upload

---
+++ to-do/app.js
@@ -0,0 +1,33 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event){
+    event.preventDefault();
+    //Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("completed-btn");
+    todoDiv.appendChild(completedButton);
+    //Trash Button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    //clear todo input box
+    todoInput.value = "";
+}
